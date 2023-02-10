eb015d7b2d4894de9846541f6cb0ced3071702c8
beznov <akksupper@gmail.com>
2023-02-10 20:19:20 +0200

    added img and comments

--- pharmacy/index.js
+++ pharmacy/index.js
@@ -6,14 +6,14 @@ $('.scroll_button').click(function(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
 
-var text = "HealthCure: Your Partner in Better Health";
-var title_text = $(".about_title");
-var index = 0;
+var text_about = "HealthCure: Your Partner in Better Health";
+var title_about = $(".about_title");
+var index_about = 0;
 
 setInterval(function() {
-    if (index < text.length) {
-        title_text.append(text[index]);
-        index++;
+    if (index_about < text_about.length) {
+        title_about.append(text_about[index_about]);
+        index_about++;
     }
 }, 50);
 
@@ -40,3 +40,71 @@ setInterval(function(){
         arrow_move = true;
     }
 }, 1000)
+
+var reached_pill_title = false;
+
+var text_pill = "VitalBoost";
+var title_pill = $(".pill_title");
+var index_pill = 0;
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() >= 100 && !reached_pill_title) {
+        setInterval(function() {
+            if (index_pill < text_pill.length) {
+                title_pill.append(text_pill[index_pill]);
+                index_pill++;
+            }
+        }, 75);
+      reached_pill_title = true;
+    }
+  });
+  
+
+  var reached_pill_img = false;
+
+  var images = [
+    "img/pill1.jpg",
+    "img/pill2.webp",
+    "img/pill3.webp",
+    "img/pill4.jpg",
+    "img/pill5.jpg"
+  ];
+
+  var i = 0;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 200 && !reached_pill_img) {
+        setInterval(function() {
+            $(".pill_img").fadeOut(300, function() {
+                $(this).attr("src", images[i]).fadeIn(300);
+              });
+            i = (i + 1) % images.length;
+        }, 5000);
+      reached_pill_img = true;
+    }
+  });
+
+var counter = 0;
+
+$('.comments_button').click(function() {
+    var comments = document.getElementsByClassName("comment");
+    counter += 3;
+    var isShowing = true;
+    if(isShowing) {
+        if (counter <= comments.length) {
+            $(this).text('Show more');
+            for (let i=0; i<counter; i++) {
+            comments[i].style.display = "block";
+            }
+        } if (counter == comments.length) {
+            $(this).text('Hide comments');
+        }
+    } if(counter > comments.length) {
+        for (let i=0; i<comments.length; i++) {
+            comments[i].style.display = "none";
+        }
+        isShowing = false;
+        counter = 0;
+        $(this).text('Show comments');
+        }
+})
